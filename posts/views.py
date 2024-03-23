4d705fca170f3137afce2e38f6f3836c2cd79911
Ujjawal Saini <spignelon@protonmail.com>
2024-03-23 20:20:05 +0530

    category highlight on sidebar

--- posts/views.py
+++ posts/views.py
@@ -10,9 +10,16 @@ from django.contrib import messages
 def home_view(request, tag=None):
     if tag:
         posts = Post.objects.filter(tags__slug=tag)
+        tag = get_object_or_404(Tag, slug=tag)
     else:
         posts = Post.objects.all()
-    return render(request, "posts/home.html", {"posts": posts})
+    categories = Tag.objects.all()
+    context = {
+        "posts": posts,
+        "categories": categories,
+        "tag": tag
+    }
+    return render(request, "posts/home.html", context)
 
 
 def post_create_view(request):
