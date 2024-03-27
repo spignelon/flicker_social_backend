0543abf423aaf9b45f3a8c9dd5de65909337dd8e
Ujjawal Saini <spignelon@protonmail.com>
2024-03-28 00:25:22 +0530

    added profile to urls and views and made a template of it

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from posts.views import *
+from users.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -29,6 +30,7 @@ urlpatterns = [
     path("post/delete/<pk>/", post_delete_view, name="post-delete"),
     path("post/edit/<pk>/", post_edit_view, name="post-edit"),
     path("post/<pk>/", post_page_view, name="post"),
+    path("profile/", profile_view, name="profile")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
