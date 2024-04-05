aa0bb92c7b50a58ea08ce78498e7d01bb62281eb
Ujjawal Saini <spignelon@protonmail.com>
2024-04-05 18:34:09 +0530

    added user profile pages on feed

--- users/views.py
+++ users/views.py
@@ -1,18 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
 from .forms import *
 
-def profile_view(request):
-    profile = request.user.profile
-    return render(request, "users/profile.html", {"profile" : profile})
+
+def profile_view(request, username=None):
+    if username:
+        profile = get_object_or_404(User, username=username).profile
+    else:
+        profile = request.user.profile
+    return render(request, "users/profile.html", {"profile": profile})
+
 
 def profile_edit_view(request):
     form = ProfileForm(instance=request.user.profile)
-    
+
     if request.method == "POST":
         form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
         if form.is_valid():
             form.save()
             return redirect("profile")
-    
-    return render(request, "users/profile_edit.html", {"form" : form})
 
+    return render(request, "users/profile_edit.html", {"form": form})
