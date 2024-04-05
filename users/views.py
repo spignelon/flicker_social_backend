d83e88564303cb7dd401eb222067b1166713d734
Ujjawal Saini <spignelon@protonmail.com>
2024-04-05 19:41:44 +0530

    added profile delete view and fixed all the permissions for editing and deleting post, now only authors can perform these actions

--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.http import Http404
 from .forms import *
 
@@ -15,6 +18,7 @@ def profile_view(request, username=None):
     return render(request, "users/profile.html", {"profile": profile})
 
 
+@login_required
 def profile_edit_view(request):
     form = ProfileForm(instance=request.user.profile)
 
@@ -25,3 +29,16 @@ def profile_edit_view(request):
             return redirect("profile")
 
     return render(request, "users/profile_edit.html", {"form": form})
+
+
+@login_required
+def profile_delete_view(request):
+    user = request.user
+    
+    if request.method == "POST":
+        logout(request)
+        user.delete()
+        messages.success(request, "Account deleted, sorry to see you go.")
+        return redirect("home")
+    
+    return render(request, "users/profile_delete.html")
